c696ace2377f2c6f49d9b23295367a6905c12a94
aturi <angelaturi99@gmail.com>
2021-04-23 17:37:31 -0400

    Work on todo list

--- skeleton/src/todo_list.js
+++ skeleton/src/todo_list.js
@@ -1,2 +1,28 @@
+const todos = [];
+const ulTag = document.querySelector('.todos')
+const addTodos = document.querySelector('.add-todo-form')
 
+const addTodo = (e) => {
+    let input = document.querySelector("input[name='add-todo']");
+    let val = input.value;
+    let todo = {
+        val,
+        done: false
+    };
+
+    todos.push(todo);
+    e.currentTarget.reset();
+
+}
+
+function populateList (todos, ulTag) {
+    todos.map((todo) => {
+        return `
+        <li>
+            <label>${todo.val}</label>
+            <input type="checkbox" ${todo.done} ? 'checked' : ''} />
+
+        </li>`
+    })
+}
 
